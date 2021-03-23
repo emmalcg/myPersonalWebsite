5ca4fb005a38ae08f3f1df4d340bd984fabdfa84
emma <emmalcgreen@gmail.com>
2021-03-23 09:40:19 -0400

    changed to noscroll on loading and menu open, must add click listenings to nav items for mobile menu

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
+document.querySelector('html').classList.add('no-scroll');
+
 setTimeout(function () {
         document.querySelector('.loader').classList.add('hidden');
+        document.querySelector('html').classList.remove('no-scroll');
     }, 2500)
 
 const navSlide = () => {
-    const burger = document.querySelector('.burger');
+    const mobileNav = document.querySelector('.mobile-nav');
     const navUlEl = document.querySelector('.main-nav');
     const navLi = document.querySelectorAll('.main-nav li');
     const circle = document.querySelectorAll('.circle');
+    
+    const htmlEL = document.querySelector('html');
 
+    
     const h1El = document.querySelector('h1 a');
     const h1Top = h1El.offsetTop; 
+
 
     const mq = window.matchMedia('(min-width: 750px)');
 
@@ -25,9 +32,11 @@ const navSlide = () => {
 
     window.addEventListener('scroll', stickyHeader);
 
-    burger.addEventListener('click', function (){
+    mobileNav.addEventListener('click', function (){
         //animate burger icon
-        burger.classList.toggle('open');
+        mobileNav.classList.toggle('open');
+
+        htmlEL.classList.toggle('no-scroll');
         //have nav appear
         navUlEl.classList.toggle('nav-open');
 
